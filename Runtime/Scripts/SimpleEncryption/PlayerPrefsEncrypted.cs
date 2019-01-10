ead74cdcb196e7243c3e1528b2332a02fd66963c
NamNH <nam.nguyen>
2019-01-10 14:31:14 +0700

    *first commit

---
+++ Runtime/Scripts/SimpleEncryption/PlayerPrefsEncrypted.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Globalization;
+using UnityEngine;
+
+namespace Assets.SimpleEncryption
+{
+	/// <summary>
+	/// A wrapper over PlayerPrefs that provides protected data storage.
+	/// </summary>
+    public static class PlayerPrefsEncrypted
+	{
+		private const string Key = "Key";
+
+        public static bool HasKey(string key)
+        {
+            return PlayerPrefs.HasKey(Md5.ComputeHash(key));
+        }
+
+        public static string GetString(string key)
+        {
+            key = Md5.ComputeHash(key);
+
+            return PlayerPrefs.HasKey(key) ? B64X.Decrypt(PlayerPrefs.GetString(key), Key) : null;
+        }
+
+        public static void SetString(string key, string value)
+        {
+	        if (value == null)
+	        {
+		        PlayerPrefs.DeleteKey(Md5.ComputeHash(key));
+			}
+	        else
+	        {
+				PlayerPrefs.SetString(Md5.ComputeHash(key), B64X.Encrypt(value, Key));
+			}
+
+            PlayerPrefs.Save();
+        }
+
+        public static int GetInt(string key)
+        {
+            return int.Parse(GetString(key) ?? "0");
+        }
+
+        public static void SetInt(string key, int value)
+        {
+            SetString(key, value.ToString());
+        }
+
+        public static bool GetBool(string key)
+        {
+            return GetString(key) != null;
+        }
+
+        public static void SetBool(string key, bool value)
+        {
+            if (value)
+            {
+                SetString(key, UnityEngine.Random.Range(0, 999999).ToString());
+            }
+            else
+            {
+                PlayerPrefs.DeleteKey(Md5.ComputeHash(key));
+                PlayerPrefs.Save();
+            }
+        }
+
+        public static DateTime GetDateTime(string key)
+        {
+            var value = GetString(key);
+
+            return value == null ? DateTime.MinValue : DateTime.Parse(value);
+        }
+
+        public static void SetDateTime(string key, DateTime value)
+        {
+            SetString(key, value.ToString(CultureInfo.InvariantCulture));
+        }
+
+        public static void DeleteKey(string key)
+        {
+            PlayerPrefs.DeleteKey(Md5.ComputeHash(key));
+            PlayerPrefs.Save();
+        }
+    }
+}
---
+++ Samples/Example.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.SimpleEncryption
+{
+    /// <summary>
+    /// Usage example.
+    /// </summary>
+    public class Example : MonoBehaviour
+    {
+        public Text Text;
+
+        /// <summary>
+        /// Execute on start.
+        /// </summary>
+        public void Start()
+        {
+            const string sample = "Hello, world!";
+
+	        string publicKey, privateKey;
+
+			RSA.GenerateKeys(out publicKey, out privateKey);
+
+	        var signature = RSA.SignData(sample, privateKey);
+
+			Text.text = string.Format("Plain text: {0}\nMd5 hash: {1}\nBase64 encoding: {2}\nB64R encoding: {3}\nB64X encryption: {4}\nAES encryption: {5}\nRSA encryption: {6}\nRSA signature: {7}\nRSA signature check: {8}",
+                sample,
+                Md5.ComputeHash(sample),
+                Base64.Encode(sample),
+                B64R.Encode(sample),
+                B64X.Encrypt(sample, "password"),
+                AES.Encrypt(sample, "password"),
+				RSA.EncryptText(sample, publicKey),
+				signature,
+				RSA.CheckSignature(sample, signature, publicKey) ? "valid" : "invalid");
+        }
+    }
+}
